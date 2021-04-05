178b6449eaed721d7868b4294f3a01cedc1bcee0
lkochappan <79967291+lkochappan@users.noreply.github.com>
2021-04-05 14:42:33 +1000

    Still not working??!

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.set('view engine', 'ejs'); 
 
 // connecting to mongodb database: Books
-mongoose.connect("mongodb+srv://leo:calligraphy004@ses1a.kdj8l.mongodb.net/Books", { useNewUrlParser: true }, { useUnifiedTopology: true }); 
+mongoose.connect("mongodb+srv://leo:calligraphy004@ses1a.kdj8l.mongodb.net/Books?retryWrites=true&w=majority", { useNewUrlParser: true }, { useUnifiedTopology: true }); 
 
 // creating a book schema
 const bookSchema = {
@@ -27,7 +27,7 @@ app.get('/', (req,res) => {
     })
 })
 
-// posting app
+
 app.listen(4000, function() {
     console.log('Server is running !');
 }); 
