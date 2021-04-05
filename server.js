3266e5b7953c39ed3fdfd607166c8cb7b4d74c6a
lkochappan <79967291+lkochappan@users.noreply.github.com>
2021-04-05 12:24:22 +1000

    trying to get nodemon to work

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express'); 
+const mongoose = require('mongoose'); 
+const ejs = require('ejs'); 
+const app = express(); 
+
+app.set('view engine', 'ejs'); 
+
+mongoose.connect('mongodb+srv://leo:calligraphy004@ses1a.kdj8l.mongodb.net/Books?retryWrites=true&w=majority'); 
+
+app.get('/', (req,res) => {
+    res.send('working')
+})
+
+app.listen(4000, function() {
+    console.log('Server is running !');
+}); 
+
