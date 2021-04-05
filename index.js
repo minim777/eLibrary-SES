1be18cf768164eedff41e1672984910acf0c8390
Kevin le <gomodagic@hotmail.com>
2021-04-05 20:55:18 +1000

    login

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Connecting to our database
-mongoose.connect('mongodb+srv://leo:calligraphy004@ses1a.kdj8l.mongodb.net/test', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://leo:calligraphy004@ses1a.kdj8l.mongodb.net/eLMS', { useNewUrlParser: true, useUnifiedTopology: true });
 
 //Schemas
 var userSchema = new mongoose.Schema({
@@ -41,13 +41,54 @@ var userSchema = new mongoose.Schema({
 var user = mongoose.model('Users', userSchema);
 
 //APIs
-app.post("/register", (req, res) => {
-  var myData = new user(req.body);
-  myData.save()
-    .then(item => {
-      res.send("item saved to database");
-    })
-    .catch(err => {
-      res.status(400).send("unable to save to database");
+// app.post("/register", (req, res) => {
+//   var myData = new user(req.body);
+//   myData.save()
+//     .then(item => {
+//       res.send("item saved to database");
+//     })
+//     .catch(err => {
+//       res.status(400).send("unable to save to database");
+//     });
+// });
+app.post('/register', async (req, res) => {
+	const { userId, password, email } = req.body
+
+	if (password.length < 5) {
+		return res.json({
+			status: 'error',
+			error: 'Password too small. Should be atleast 6 characters'
+		})
+	}
+
+	try {
+		const response = await user.create({
+			userId,
+			password,
+      email
+		})
+		console.log('User created successfully: ', response)
+	} catch (error) {
+		if (error.code === 11000) {
+			// duplicate key
+			return res.json({ status: 'error', error: 'ID already in use' })
+		}
+		throw error
+	}
+  res.redirect('../login');
+})
+
+app.post("/login", (req, res) => {
+  var MongoClient = require('mongodb').MongoClient;
+  var url = 'mongodb+srv://leo:calligraphy004@ses1a.kdj8l.mongodb.net/eLMS';
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("eLMS");
+    dbo.collection("users").findOne({}, function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      console.log('Login success');
+      db.close();
     });
+  });
 });
