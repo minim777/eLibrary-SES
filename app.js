af7a87a54b94e9d52f16858ae4f73020b3dcc5f9
lkochappan <79967291+lkochappan@users.noreply.github.com>
2021-04-07 14:06:03 +1000

    User Stories ALMOST WORKING git add .git add .

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Importing important modules
 const express = require('express'); 
 const mongoose = require('mongoose'); 
 const ejs = require('ejs'); 
@@ -5,6 +6,7 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
+// Setting the view engine to ejs
 app.set('view engine', 'ejs'); 
 app.use(express.static('public')); 
 
@@ -18,20 +20,22 @@ const dbName = 'eLMS';
 // Create a new MongoClient
 const client = new MongoClient(url);
 
-// Use connect method to connect to the Server
+// Use connect method to connect to the Server --> Make sure that the Database 
+// Then set up the actual server.
 client.connect(function(err) {
   assert.equal(null, err);
   console.log("Connected successfully to db");
 
   app.listen(4000, function() {
     console.log('Server is running !');
-    //setTimeout( () => {
-        //console.log(mongoose.connection.readyState); // prints 1
-      //}, 2000);
+    setTimeout( () => {
+        console.log(mongoose.connection.readyState); // prints 1
+      }, 2000);
 }); 
 
 });
 
+// Selecting the database and the right collection, taking all of the data in it and putting it into a list
 app.get('/', (req,res) => {
 
     const db = client.db(dbName);
