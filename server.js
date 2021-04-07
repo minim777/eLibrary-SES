f04f9f92db196dfb7b4ad53d512b12af25d9430c
lkochappan <79967291+lkochappan@users.noreply.github.com>
2021-04-07 11:30:55 +1000

    Search Bar work in progress

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express'); 
-//const mongoose = require('mongoose'); 
+const mongoose = require('mongoose'); 
 const ejs = require('ejs'); 
 const app = express(); 
 const MongoClient = require('mongodb').MongoClient;
@@ -7,10 +7,6 @@ const assert = require('assert');
 
 app.set('view engine', 'ejs'); 
 app.use(express.static('public')); 
-
-// connecting to mongodb database: Books
-//mongoose.connect('mongodb+srv://leo:calligraphy004@ses1a.kdj8l.mongodb.net/Books?retryWrites=true&w=majority', { useNewUrlParser: true }, { useUnifiedTopology: true }); 
-
 
 
 // Connection URL
@@ -36,24 +32,7 @@ client.connect(function(err) {
 
 });
 
-// creating a book schema
-const bookSchema = {
-    title: String, 
-    author: String, 
-    category: String, 
-    type: String
-}
-
-// creating data model
-//const Book = mongoose.model('Book', bookSchema); 
-
 app.get('/', (req,res) => {
-    //Book.find({}, function(err, Books) {
-        //res.render('BrowseBooks', {
-            //BooksList: Books
-            //user: 'leo'
-        //})
-    //})
 
     const db = client.db(dbName);
     const collection = db.collection('Books');
@@ -64,17 +43,6 @@ app.get('/', (req,res) => {
         console.log(books_list)
         res.render('BrowseBooks', {Books: books_list})
      });
-
-
-
-
-
-
-
-
-
-
-
 })
 
 
