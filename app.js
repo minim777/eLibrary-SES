9645238a31ef3f1be2a2c0ca0fe485b44305c047
pakkamall <deepaqdmaalampali@gmail.com>
2021-04-28 19:58:13 +1000

    removed buggy hmtl attributes

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ db.on('error', function(err) {
 // Load/Register View Engine
 app.set('/views/', path.join(__dirname, '/views/'));
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, '/public/adminmenu/')))
+app.use(express.static(path.join(__dirname, '/public/')))
 
 // Home Route
 app.get('/', function(req, res){
