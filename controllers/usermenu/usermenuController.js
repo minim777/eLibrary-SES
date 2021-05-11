f7fc1af33e6be3a497b3abe000b4d7653f72b1a4
Leo Kochappan <leo.kochappan@gmail.com>
2021-05-12 09:59:31 +1000

    issues with loading

--- controllers/usermenu/usermenuController.js
+++ controllers/usermenu/usermenuController.js
@@ -15,34 +15,7 @@ const menu_home = function(req, res){
     });
 }
 
-const book_page = function(req, res){                       // work in progress
-    Book.findOne({}, function(err, book){
-        if(err){
-            console.log(err);
-        }
-        else {
-            res.render('usermenu/borrow', {
-            title: "Borrow a book",
-            book: book
-            });
-        }
-    });
-}
-// const borrow_one = function(req, res){
-//     if(err){
-//         console.log(err);
-//     }
-//     Book.findById(req.params.id, function(err, book){
-//         res.render('usermenu/borrow', {
-//            title: "Borrow a Book",
-//            book: book
-//          });
-//        });
-// }
-
 
 module.exports = {
     menu_home,
-    book_page
-
 }
